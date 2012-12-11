639e207c4dff62ec31f650e713103e950a69f493
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2012-12-10 23:20:31 -0500

    fix bug in create_date_variables; update version; dc flag into inst/ directory

--- R/create_date_variables.R
+++ R/create_date_variables.R
@@ -9,7 +9,7 @@ create_date_variables <- function(crime_data) {
   crime_data$date <- as.Date(gsub(" .*", "", crime_data$REPORT_DAT), format='%m/%d/%Y')
   crime_data$year <- format(crime_data$date, format='%Y')
   crime_data$month <- format(crime_data$date, format='%B')
-  crime_data$quarter <- as.character(ceiling(crime_data$month %% 3))
+  crime_data$quarter <- as.character(ceiling(as.integer(format(crime_data$date, '%m')) / 3))
 
   crime_data$year_month <- sprintf("%s-%s", crime_data$year, format(crime_data$date, '%b'))
   crime_data$year_quarter <- sprintf("%s-Q%s", crime_data$year, crime_data$quarter)
